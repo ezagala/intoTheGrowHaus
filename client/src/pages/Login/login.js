5095a2c3fac3305040b536f7dd40f48d6325cb67
Eric <ejzagala@gmail.com>
2018-07-23 20:13:21 -0600

    replace package-lock w/ yarn.lock:

--- client/src/pages/Login/login.js
+++ client/src/pages/Login/login.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import {Redirect} from "react-router-dom";
-
 // import API from "../../utils/API";
 import "./Login.css"
 import { Col, Row, Container } from "../../components/Grid";
