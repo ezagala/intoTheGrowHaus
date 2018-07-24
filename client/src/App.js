c0dc96f6eeab88e395bba58e00dc8296d5a1920f
Eric <ejzagala@gmail.com>
2018-07-23 21:33:22 -0600

    hello?

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./pages/Login";
+import Login from "./pages/Login/Login";
 import Search from "./pages/Search";
 import NoMatch from "./pages/NoMatch";
 // import Nav from "./components/Nav";
