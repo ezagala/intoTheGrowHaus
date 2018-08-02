c47e0281831f10222e9c9d0bdef73713a8fcedc6
Eric <ejzagala@gmail.com>
2018-08-01 18:16:47 -0600

    chart progess

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import c3 from "c3";
 // import {Redirect} from "react-router-dom";
 import DatePicker from 'react-date-picker';
-import { FormGroup, ControlLabel, FormControl, ListGroup, ListGroupItem} from 'react-bootstrap';
+import { FormGroup, ControlLabel, FormControl, ListGroup, ListGroupItem } from 'react-bootstrap';
 import moment from 'moment';
 import API from "../../utils/API";
 import "./Search.css"
@@ -10,74 +11,80 @@ import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 import { SearchBtn } from "../../components/Form";
 
-class Search extends Component {    
+
+class Search extends Component {
     state = {
         customer: '',
         startDate: '',
         endDate: '',
-        transactions: [], 
+        transactions: [],
         columns: [
-            ['x'], 
+            ['x'],
             ['data1']
         ]
     };
 
     componentDidMount() {
-        API.getTransactions()
-            .then(res => {
-
-                // Define first and last transations of the data set 
-                let firstDate = res.data[res.data.length -1].date; 
-                let lastDate = res.data[0].date; 
-                // Define initial sales total for a specific date
-                let dayTotal = 0;
-                // Define a the first value for the curent day 
-                let currentDate = moment(firstDate).format("YYYY/DD/MM"); 
-
-                res.data.forEach(trans => {
-                    // if the transaction was made a specific day, add it to the total of transactions for that day, then push that total to columns as data1 array
-                    
-                    if (moment(trans.date).format("YYYY/DD/MM") === currentDate) {
-                        dayTotal += trans.transTotal; 
-                    } else {
-                        // Update data1
-                        if (dayTotal !== 0) this.state.columns[1].push(dayTotal); 
-                        // reset dayTotal
-                        dayTotal = 0; 
-                        // Set currentDate to the new date 
-                        currentDate = moment(trans.date).format("YYYY/DD/MM"); 
-                        // Update day total with the first transTotal for that day
-                        dayTotal += trans.transTotal; 
-                        
-                    }
-                    // (if the transaction was made on a date between the first and last transaction) && (the date is divisible by 5) && (the date does not already exist in the array), then push that (formatted) date to the columns x array 
-                    if ((trans.date > firstDate) && (trans.date < lastDate)) {
-                        let xTickTest = moment(trans.date).format("DD");
-                        let xTick = moment(trans.date).format("YYYY/DD/MM"); 
-                        if ((xTickTest % 5 === 0) && (this.state.columns[0].indexOf(xTick) === -1)) {
-                            this.state.columns[0].push(xTick); 
-                        }
-                    }
-                })
-                console.log("data1: ", this.state.columns[1]); 
-                console.log("x axis: ", this.state.columns[0]); 
-            }); 
+        this.renderTrend(); 
     }
 
     // Will render a chart that shows the trend of daily sales totals for the entire history
     renderTrend = () => {
-        // API.getTransactions()
-        //     .then(res => {
+        API.getTransactions()
+            .then(res => {
 
-        //         res.data.forEach(trans => {
-        //             // if the transaction was made a specific day, add it to the total of transaction for that day
-        //                 // push that total to columns as data1 array
-                    
-                    
-        //         })
+                // Define first and last transations of the data set 
+                let firstDate = res.data[res.data.length - 1].date;
+                let lastDate = res.data[0].date;
+                // Define initial sales total for a specific date
+                let dayTotal = 0;
+                // Define a the first value for the curent day 
+                let currentDate = moment(firstDate).format("YYYY/DD/MM");
 
-        //         console.log(res.data[450])
-        //     }); 
+                res.data.forEach(trans => {
+                    // if the transaction was made a specific day, add it to the total of transactions for that day, then push that total to columns as data1 array
+
+                    if (moment(trans.date).format("YYYY/DD/MM") === currentDate) {
+                        dayTotal += trans.transTotal;
+                    } else {
+                        // Update data1
+                        if (dayTotal !== 0) this.state.columns[1].push(dayTotal);
+                        // reset dayTotal
+                        dayTotal = 0;
+                        // Set currentDate to the new date 
+                        currentDate = moment(trans.date).format("YYYY/DD/MM");
+                        // Update day total with the first transTotal for that day
+                        dayTotal += trans.transTotal;
+
+                    }
+                    // (if the transaction was made on a date between the first and last transaction) && (the date is divisible by 5) && (the date does not already exist in the array), then push that (formatted) date to the columns x array 
+                    if ((trans.date > firstDate) && (trans.date < lastDate)) {
+                        let xTickTest = moment(trans.date).format("DD");
+                        let xTick = moment(trans.date).format("YYYY/DD/MM");
+                        if ((xTickTest % 5 === 0) && (this.state.columns[0].indexOf(xTick) === -1)) {
+                            this.state.columns[0].push(xTick);
+                        }
+                    }
+                })
+                console.log("data1: ", this.state.columns[1]);
+                console.log("x axis: ", this.state.columns[0]);
+            });
+
+        const chart = c3.generate({
+            bindto: '#chart',
+            data: {
+                x: "Year", 
+                columns: this.state.columns
+            }, 
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%m/%d'
+                    }
+                }
+            }
+        })
     }
 
     loadTransactions = data => {
@@ -86,7 +93,7 @@ class Search extends Component {
             this.setState({ transactions: data });
             console.log("State.transactions upated to: ", this.state.transactions);
         }
-        
+
     };
 
     onStartDateChange = startDate => this.setState({ startDate });
@@ -102,41 +109,41 @@ class Search extends Component {
     handleFormSubmit = event => {
         event.preventDefault();
         API.query({
-            startDate: this.state.startDate, 
-            endDate: this.state.endDate,    
+            startDate: this.state.startDate,
+            endDate: this.state.endDate,
             customer: this.state.customer.toString()
         })
-            .then( res => {
+            .then(res => {
                 const transactions = res.data
-            
+
                 // for (const data in transactions) {
                 //     transactions.push(transactions[data]); 
                 // }
- 
+
                 this.loadTransactions(transactions);
-                console.log("Response: ", transactions); 
+                console.log("Response: ", transactions);
             })
-            .catch(err => console.log(err)); 
+            .catch(err => console.log(err));
 
 
     };
 
     render() {
         return (
-                
+
             <div>
                 <Nav onClick={this.logout} />
                 <Container fluid>
-                <Row>
-                    <Col size="md-12">
-                        {
-                            this.state.columns[0].length < 1 ? 
-                            (<div id="chart"></div>) 
-                                : 
-                            (<span>{/*Chart will not be render if there is no chart data*/}</span>)
-                        }
-                    </Col> 
-                </Row> 
+                    <Row>
+                        <Col size="md-12">
+                            {
+                                this.state.columns[0].length < 1 ?
+                                    (<div id="chart"></div>)
+                                    :
+                                    (<span>{/*Chart will not be render if there is no chart data*/}</span>)
+                            }
+                        </Col>
+                    </Row>
                     <Row>
                         <Col size="md-6">
                             <div className="searchForm">
@@ -145,11 +152,11 @@ class Search extends Component {
                                 <form>
                                     <FormGroup controlId="customerSelect">
                                         <ControlLabel>Customer Type</ControlLabel>
-                                        <FormControl 
-                                            value={this.state.customer} 
+                                        <FormControl
+                                            value={this.state.customer}
                                             onChange={this.handleInputChange}
                                             name="customer"
-                                            componentClass="select" 
+                                            componentClass="select"
                                             placeholder="select">
                                             <option value="select">Select...</option>
                                             <option value="1">Zone One</option>
@@ -159,12 +166,12 @@ class Search extends Component {
                                     </FormGroup>
                                     <FormGroup controlId="loyaltyCustomer"  >
                                         <ControlLabel>Or:  Loyalty Customer</ControlLabel>
-                                        <FormControl 
-                                            value={this.state.customer} 
-                                            onChange={this.handleInputChange}  
+                                        <FormControl
+                                            value={this.state.customer}
+                                            onChange={this.handleInputChange}
                                             name="customer"
-                                            type="text" 
-                                            placeholder="Phone Number (like 3335557777)" 
+                                            type="text"
+                                            placeholder="Phone Number (like 3335557777)"
                                         />
                                     </FormGroup>
                                     <p>Between dates</p>
@@ -197,7 +204,7 @@ class Search extends Component {
                                         {this.state.transactions.map(transaction => (
                                             <ListGroupItem key={transaction._id}>
                                                 <h4>Transaction ID: {transaction.transID}</h4>
-                                                <hr/> 
+                                                <hr />
                                                 <ListGroup>
                                                     <ListItem>- Customer: {transaction.customer}</ListItem>
                                                     <ListItem>- Date: {moment(transaction.date).format("MM/DD/YYYY")}</ListItem>
